2077f52d1521fe4b9a8f513e2814e8fef01904af
Dejan Demonjić <dejan.demonjic@tridesetri.com>
2025-05-21 06:32:10 +0200

    _index files added

---
+++ examples/Osirion.Blazor.Example.Bootstrap/Components/Pages/Home.razor.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components;
+using Osirion.Blazor.Cms.Domain.Entities;
+using Osirion.Blazor.Cms.Domain.Services;
+
+namespace Osirion.Blazor.Example.Bootstrap.Components.Pages;
+
+public partial class Home(IContentProviderManager ContentProviderManager) 
+{
+    [Parameter]
+    public string? Locale { get; set; } = "en";
+
+    private ContentItem? Content { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await LoadContentAsync();
+    }
+
+    private async Task LoadContentAsync()
+    {
+        var provider = ContentProviderManager.GetDefaultProvider();
+        if (provider == null)
+            return;
+
+        var result = await provider.GetItemsByQueryAsync(new Cms.Domain.Repositories.ContentQuery { Locale = Locale, Slug = "getting-started-with-osirion-blazor" });
+        Content = result?.FirstOrDefault();
+    }
+}
